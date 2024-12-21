91c35809e2491444475b150b654756d5c4a85491
‌‌‌‌✘ 𝗞𝗔𝗪𝗗𝗛𝗜𝗧𝗛𝗔 𝗡𝗜𝗥𝗠𝗔𝗟 •| <141761993+KAWDHITHA-NIRMAL@users.noreply.github.com>
2024-12-21 23:21:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,15 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "wH5ixTxa#sx2cr5qZpW8PlFzkDnil2flVqsQa7UblDlCELKBRzv0",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/198a322e8cc0f2bd6cba2.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94765527900",
-SUDO_NB: process.env.SUDO_NB || "94718913389",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-MODE: process.env.MODE || "public",
-AUTO_VOICE:"flase"
+    SESSION_ID: process.env.SESSION_ID || "wH5ixTxa#sx2cr5qZpW8PlFzkDnil2flVqsQa7UblDlCELKBRzv0",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/198a322e8cc0f2bd6cba2.jpg",
+    ALIVE_MSG: process.env.ALIVE_MSG || "🤖🔰 Hi QUEEN-KYLIE-MD Is Online Now 💻\n*💻 Owner* - QUEEN-KYLIE-MD\n\n*💻 Owner Number* -94765527900",
+    SUDO_NB: process.env.SUDO_NB || "94718913389",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+    MODE: process.env.MODE || "public",
+    AUTO_VOICE: process.env.AUTO_VOICE || "false",
+    LOGO: process.env.LOGO || "https://telegra.ph/file/091fc81528af5881cdf47.jpg", // Default LOGO URL
+    FOOTER: process.env.FOOTER || "🎬 QUEEN-KYLIE-MD MOVIEDL 🎬" // Default Footer Text
 };
